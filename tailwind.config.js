ffc71a3474efd0941749e5d71c51c73b5796c279
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2024-03-19 16:39:49 +0100

    version-5

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        body: ['Nunito']
+      }
+    },
   },
   plugins: [],
 }
